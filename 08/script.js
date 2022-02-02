f94bb30b2a3ec7ffc2e865e67609f4e52ddfe5ac
Vladimirovilya <vladimirov.ilya@kmrf.kiev.ua>
2022-02-02 14:38:29 +0200

    fix

---
+++ 08/script.js
@@ -0,0 +1,13 @@
+function convert() {
+    let Cs = prompt('Введіть кількість градусів Цельсія:');
+    let Celsiy = Number(Cs);
+    console.log(typeof(Celsiy));
+
+    let pounds = Cs  * 9/5+32;
+    let ounces = Cs / 273;
+    
+
+    alert(`${Celsiy} градусів = ${pounds.toFixed(2)} Фаренгейт`);
+    alert(`${Celsiy} градусів = ${ounces.toFixed(2)} Кельвін`);
+    
+}
