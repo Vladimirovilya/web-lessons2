9f3281d2aa608b19b49403a6127392a6cc89e3aa
Vladimirovilya <vladimirov.ilya@kmrf.kiev.ua>
2022-06-13 22:41:27 +0300

    masiv

---
+++ Tekst_Documents/script.js
@@ -0,0 +1,11 @@
+document.write('Завдання 1<br>');
+
+document.write('<i>Завдання 2</i><br>');
+
+let str = '<i>Завдання 3</i><br>';
+document.write(str);
+
+document.write('Завдання 4:<br>');
+for (var i = 1; i <= 5; i++) {
+	document.write(i + '<br>');
+}
